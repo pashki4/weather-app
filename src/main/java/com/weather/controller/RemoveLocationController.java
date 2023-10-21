183379b5f4ed3507184654acfc952d506cde8214
PashaVyshniakov <pashki4@gmail.com>
2023-10-21 16:45:36 +0300

    fixed views after added weatherData

--- src/main/java/com/weather/controller/RemoveLocationController.java
+++ src/main/java/com/weather/controller/RemoveLocationController.java
@@ -2,8 +2,12 @@ package com.weather.controller;
 
 import com.weather.config.ThymeleafConfiguration;
 import com.weather.dao.UserDAO;
+import com.weather.dto.UserDto;
+import com.weather.model.Location;
 import com.weather.model.User;
+import com.weather.service.HttpService;
 import com.weather.service.UserService;
+import com.weather.util.MapperUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,25 +19,40 @@ import org.thymeleaf.web.IWebExchange;
 import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 @WebServlet("/remove")
 public class RemoveLocationController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        Long userId = Long.valueOf(req.getParameter("userId"));
-        Long locationId = Long.valueOf(req.getParameter("locationId"));
-
-        UserService userService = new UserService(new UserDAO());
-        userService.removeLocation(userId, locationId);
-        Optional<User> user = userService.getById(userId);
         TemplateEngine templateEngine = (TemplateEngine) getServletContext().getAttribute(
                 ThymeleafConfiguration.TEMPLATE_ENGINE_ATTR);
         IWebExchange webExchange = JakartaServletWebApplication.buildApplication(getServletContext())
                 .buildExchange(req, resp);
         WebContext context = new WebContext(webExchange);
 
-        context.setVariable("user", user.get());
+        Long userId = Long.valueOf(req.getParameter("userId"));
+        Long locationId = Long.valueOf(req.getParameter("locationId"));
+
+        UserService userService = new UserService(new UserDAO());
+        userService.removeLocation(userId, locationId);
+        Optional<User> optionalUser = userService.getById(userId);
+        User user = optionalUser.get();
+        UserDto userDto = MapperUtil.mapUserDto(user);
+
+        userDto.locations.forEach(location -> {
+            String latitude = String.valueOf(location.getLatitude());
+            String longitude = String.valueOf(location.getLongitude());
+            String weatherDataUrl = HttpService.createWeatherDataUrl(latitude, longitude);
+            HttpRequest weatherDataRequest = HttpService.prepareHttpRequest(weatherDataUrl);
+            location.setWeatherData(MapperUtil
+                    .mapWeatherData(HttpService.sendRequest(weatherDataRequest)));
+        });
+
+        context.setVariable("user", userDto);
         templateEngine.process("authorized", context, resp.getWriter());
     }
 }
