64509b958b57da9f34ab3557d683eb9c70442fd3
PashaVyshniakov <pashki4@gmail.com>
2023-11-02 12:16:25 +0200

    add junit5

---
+++ src/test/java/com/weather/service/UserServiceTest.java
@@ -0,0 +1,11 @@
+package com.weather.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+    @Test
+    void userServiceShouldBeSomething() {
+    }
+}
